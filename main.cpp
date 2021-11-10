2fafe4a8d2703f5204b97fd372d206ec99d377c7
S4977690 <91629502+S4977690@users.noreply.github.com>
2021-11-10 23:21:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 int main() {
-    int i=0, n=0, p=i, s=1, r=0;
+    int i=0, n=0, p=i, s=0, r=0;
     cin >> n;
-    cout << 1 << endl;
-    for (int i = 1; i <=n ; i++) {
+    for (int i = 1; i <=n+2 ; i++) {
         r = p + s;
         p = s;
         s = r;
+        if (f<=n) {
         cout << r << endl;
-
+        }
     }
     return 0;
 }
